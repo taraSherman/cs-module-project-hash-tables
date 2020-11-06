bbd7afc0ac085b61d6c0374d427aaae509ee36ab
Tara Sherman <taradelin@gmail.com>
2020-11-05 21:12:08 -0500

    expensive_sez.py

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,20 @@
 # Your code here
+#        if x <= 0:
+#             y + z
+#         if x >  0:
+#            exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
 
+table = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
-
+    keys = (x, y, z)
+    if keys not in table:
+        if x <= 0:
+            table[keys] = y + z
+        else:
+            table[keys] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return table[keys]
 
 if __name__ == "__main__":
     for i in range(10):
