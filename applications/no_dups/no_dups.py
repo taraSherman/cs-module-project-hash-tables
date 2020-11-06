de41bae4d84143682cd110b7c112dbeb64f081be
Tara Sherman <taradelin@gmail.com>
2020-11-05 22:18:26 -0500

    no_dups.py

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,19 @@
 def no_dups(s):
     # Your code here
-
+    # create dictionary
+    words = {}
+    # split string, store in variable
+    words_list = s.split()
+    # create empty array
+    no_dupes_list = []
+    # loop over
+    for word in words_list:
+        # if word not found in dictionary, add it to array
+        if word not in words:
+            words[word] = 1
+            no_dupes_list.append(word)
+    # use join on words in array to make string and return that
+    return ' '.join(no_dupes_list)
 
 
 if __name__ == "__main__":
