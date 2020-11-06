148c6e038f8c0b514d8e1b1eb9452e21383598e2
Tara Sherman <taradelin@gmail.com>
2020-11-05 21:59:48 -0500

    word_count.py

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,26 @@
-def word_count(s):
-    # Your code here
+ignore_chars = "\" : ; , . - + = / \ | [ ] { } ( ) * ^ &".split()
 
+def word_count(s):
+    sentence = {}
+    # split string into separate strings (words) on spaces
+    words = s.split()
+    # for each word
+    for word in words:
+        # make all lowercase
+        word = word.lower()
+        # for each character that is to be ignored,
+        for i in ignore_chars:
+            # remove it
+            word = word.replace(i, "")
+        # so long as input is not an ignored character, do one or the other of the following
+        if word != "":
+            # if input is empty
+            if word not in sentence:
+                # return empty
+                sentence[word] = 0
+            # if input is not empty, add and move to next
+            sentence[word] += 1
+    return sentence
 
 
 if __name__ == "__main__":
